71b8c2df86310b89195934233db889add82a0184
Kornel <kornel.tlaczala@gmail.com>
2024-11-11 17:22:59 +0100

    cleanup

--- lecture/Lecture/L04-JS/script1.js
+++
@@ -1,8 +0,0 @@
-function square(i) {
-    document.write("The call passed ", i, " to the function:" , "<P>square()<P>");
-    return i * i;
-}
-
-console.error("2222");
-//this code is interpreted and executed when the header is parsed i.e.when the document is downloaded
-document.write("The square function has returned ", square(10));
